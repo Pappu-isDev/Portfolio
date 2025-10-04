55810ba52a0d1a7deacbc9753025b6449a36aab1
Pappu-isDev <pappu.isdev@gmail.com>
2025-10-04 23:01:43 +0530

    push

--- src/components/AboutUs.jsx
+++ src/components/AboutUs.jsx
@@ -5,6 +5,8 @@ import { FaLinkedinIn } from "react-icons/fa";
 import { SiGmail } from "react-icons/si";
 const AboutUs = () => {
   const myName = process.env.NEXT_PUBLIC_NAME;
+  const gitUrl = process.env.NEXT_PUBLIC_GIT;
+  const linkUrl = process.env.NEXT_PUBLIC_LINKEDIN;
   return (
     <>
       <div className='w-full flex justify-center items-center  mx-auto text-center h-100 bg-gradient-to-b from-black via-black-200 to-gray-900 text-white'>
@@ -15,16 +17,28 @@ const AboutUs = () => {
               Hi, I'm <span>{myName}</span>, a passionate and skilled Frontend Developer with a knack for crafting visually appealing, interactive, and responsive web applications.Motivated and self-taught Front-End Developer with a strong foundation in HTML, CSS, JavaScript, and React along with hands-on experience building responsive websites and web applications through personal and academic projects. Eager to contribute to real-world development teams I specialize in turning ideas into ,digital experiences, ensuring both functionality and user satisfaction.
             </p>
           </div>
-          <div className="flex justify-center  items-center gap-10 mt-5 text-gray-500 ">
-<div className="cursor-pointer p-2 transition-all duration-300 hover:text-[#181717] hover:scale-110 hover:drop-shadow-[0_0_8px_rgb(59,130,246),0_0_15px_rgb(59,130,246),0_0_25px_rgb(59,130,246)] rounded-full flex items-center justify-center">
-  <FaGithub size={40} />
+          <div className="flex justify-center items-center gap-10 mt-5 text-gray-500 ">
+<div className="flex justify-center items-center gap-10  text-gray-500">
+  <a
+    href={gitUrl}
+    target="_blank"
+    rel="noopener noreferrer"
+    className="cursor-pointer transition-all duration-300 hover:text-[#181717] hover:scale-110 hover:drop-shadow-[0_0_8px_rgb(59,130,246),0_0_15px_rgb(59,130,246),0_0_25px_rgb(59,130,246)] rounded-full"
+  >
+    <FaGithub size={40} />
+  </a>
 </div>
-
   <div className="hover:text-[#25D366] hover:scale-110 cursor-pointer hover:drop-shadow-[0_0_8px_rgba(59,130,246,0.8)] transition-all">
     <BsWhatsapp size={40} />
   </div>
 <div className="w-13 h-13 rounded-full flex items-center justify-center cursor-pointer transition-all duration-300 text-[#0A66C2] hover:bg-[#0A66C2] hover:text-white hover:drop-shadow-[0_0_8px_#0A66C2]">
-  <FaLinkedinIn size={28} />
+  <a
+    href={linkUrl}
+    target="_blank"
+    rel="noopener noreferrer"
+  >
+    <FaLinkedinIn size={28} />
+  </a>
 </div>
 <div className="h-12 w-12 cursor-pointer transition-all duration-300 flex items-center justify-center rounded-full hover:bg-white hover:text-[#25D366] hover:scale-110 hover:drop-shadow-[0_0_8px_rgba(59,130,246,0.8)]">
   <img src="/gmail.png" alt="gmail" className="w-8 h-8 mb-1" />
@@ -34,8 +48,6 @@ const AboutUs = () => {
 </div>
 
 </div>
-
-
         </div>
       </div>
     </>
