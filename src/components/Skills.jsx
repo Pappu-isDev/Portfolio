7546a8029b0bf14e645f89dbdfb3acb636c8247d
VirenderVerma0 <virenderv111@gmail.com>
2025-10-07 00:26:57 +0530

    pull

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -3,31 +3,32 @@ import React, { useRef } from "react";
 import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { BsCardImage } from "react-icons/bs";
 
 gsap.registerPlugin(ScrollTrigger);
 
 export default function SkillsSection() {
   const sectionRef = useRef(null);
+  const naukriUrl = process.env.NEXT_PUBLIC_NAUKRI;
 
   useGSAP(() => {
     gsap.fromTo(
       ".skill-card",
-      { opacity: 0, y: 60, scale: 0.8 },
+      { opacity: 0, y: -70,  },
       {
         opacity: 1,
         y: 0,
         scale: 1,
         duration: 1,
-        ease: "back.out(1.7)",
-        stagger: 0.25,
+        ease: "bounce",
+        stagger: 0.3,
         scrollTrigger: {
-          trigger: sectionRef.current,
-          start: "top 75%",            
-          end: "bottom 40%",           
-          toggleActions: "play none none reverse", 
-          
-          markers: false, 
-        },
+          trigger: ".trigger", 
+          start: "top 95%",            
+          end: "bottom 80%",          
+          toggleActions: "play none none reverse",
+         markers:false
+        }
       }
     );
   }, []);
@@ -61,13 +62,16 @@ export default function SkillsSection() {
       <div className="max-w-5xl mx-auto text-center">
         <h2 className="text-4xl font-bold mb-12">My Skills</h2>
 
-        <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-6">
+        <div className="trigger grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-6">
           {skills.map((skill, index) => (
             <div
               key={index}
-              className="skill-card cursor-pointer bg-gray-800 rounded-xl p-6 shadow-lg text-lg font-medium hover:bg-gray-700 transition"
+              className="skill-card cursor-pointer bg-gray-800 rounded-xl p-6 shadow-lg text-lg font-medium hover:bg-gray-700 "
             >
+              <span>
+
               {skill}
+              </span>
             </div>
           ))}
         </div>
