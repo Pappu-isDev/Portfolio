8919954b9a497ea4792be45ccb4708d10115fecc
VirenderVerma0 <virenderv111@gmail.com>
2025-10-07 23:06:00 +0530

    push

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -14,14 +14,18 @@ export default function SkillsSection() {
   useGSAP(() => {
     gsap.fromTo(
       ".skill-card",
-      { opacity: 0, y: -70,  },
+      { opacity: 0, y: 80, scale: 0.8, rotationX: -40 , transformOrigin: "center bottom",duration:1.2, ease: "elastic.out(1, 0.6)",stagger: { each: 0.5, from: "start" } },
       {
         opacity: 1,
         y: 0,
         scale: 1,
-        duration: 1,
-        ease: "bounce",
-        stagger: 0.3,
+        duration: 1.2,
+        ease: "bounce.out",
+        stagger: {
+          each: 0.2,
+          from: "start",
+          
+        },
         scrollTrigger: {
           trigger: ".trigger", 
           start: "top 95%",            
@@ -59,14 +63,14 @@ export default function SkillsSection() {
       className="py-20 px-6 sm:px-12 bg-gray-900 text-white"
       id="skills"
     >
-      <div className="max-w-5xl mx-auto text-center">
+      <div className="max-w-5xl  mx-auto text-center">
         <h2 className="text-4xl font-bold mb-12">My Skills</h2>
 
-        <div className="trigger grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-6">
+        <div className="trigger  grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
           {skills.map((skill, index) => (
             <div
               key={index}
-              className="skill-card cursor-pointer bg-gray-800 rounded-xl p-6 shadow-lg text-lg font-medium hover:bg-gray-700 "
+              className="skill-card cursor-pointer bg-gray-800 rounded-xl p-6 transform translate-y-[100px] scale-0.9  shadow-lg text-lg font-medium hover:bg-gray-700 "
             >
               <span>
 
