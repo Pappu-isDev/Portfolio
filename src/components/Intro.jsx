9ef132f1a88a257f3c2f97333224ff0af5360ea3
Pappu-isDev <pappu.isdev@gmail.com>
2026-01-06 00:52:55 +0530

    project-close

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -363,7 +363,7 @@ const Intro = () => {
       {isClient && (
         <motion.div
           ref={modelRef}
-          className="absolute z-10 lg:h-106 lg:w-100 lg:ml-100 lg:-mt-100 md:h-90 md:w-80 md:ml-55 md:-mt-73 sm:h-80 sm:w-100 sm:ml-50 sm:-mt-65 h-80 w-100 -ml-5 -mt-65 "
+          className="absolute z-10 lg:h-106 lg:w-100 lg:ml-100 lg:-mt-100 md:h-90 md:w-80 md:ml-55 md:-mt-73 sm:h-80 sm:w-100 sm:ml-50 sm:-mt-65 h-80 w-100 ml-25 -mt-65 "
           initial={{ opacity: 0 }}
           animate={{ opacity: showModel ? 1 : 0 }}
           transition={{ duration: 0.5, ease: "easeOut" }}
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -36,11 +36,11 @@ const Navbar = ({ name }) => {
           <div className="text-xl sm:text-2xl font-sans font-bold">{name}</div>
 
           {/* Desktop Menu */}
-          <div className="hidden sm:block">
+          <div className="">
             <div className="flex gap-6 lg:gap-8 items-center">
               {navItems.map((item) => (
                 <a key={item.id} href={item.href} onClick={() => setIsMenuOpen(false)}>
-                  <div className="relative focus:font-bold group cursor-pointer px-1 font-sans py-2 text-base lg:text-xl text-gray-200 font-medium transition-all duration-300 hover:text-white hover:scale-105 hover:shadow-blue-500/50 hover:-translate-y-1 lg:hover:-translate-y-2">
+                  <div className="relative focus:font-bold group cursor-pointer px-1 font-sans py-2 text-base lg:text-xl text-white font-medium transition-all duration-300 hover:text-white hover:scale-105 hover:shadow-blue-500/50 hover:-translate-y-1 lg:hover:-translate-y-2">
                     {item.label}
                     <span className="absolute left-0 bottom-0 w-full h-0.5 bg-gradient-to-r from-blue-400 via-purple-500 to-pink-500 scale-x-0 group-hover:scale-x-100 origin-left transition-transform duration-500 rounded"></span>
                   </div>
